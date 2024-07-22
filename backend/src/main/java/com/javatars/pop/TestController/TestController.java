16c9cc20bd2d5b8ebbfe4eaab575f1e08c550e9f
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-22 17:44:35 +0200

    Removed jpa dependency

--- backend/src/main/java/com/javatars/pop/TestController/TestController.java
+++ backend/src/main/java/com/javatars/pop/TestController/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("")
+@RequestMapping("/api/test")
 public class TestController {
 
     @GetMapping
