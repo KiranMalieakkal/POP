9f89c208a309d79a5a0a8a7e183bb980edc30c81
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-22 16:46:11 +0200

    Setting Up Backend for POP

---
+++ backend/src/main/java/com/javatars/pop/PopApplication.java
@@ -0,0 +1,13 @@
+package com.javatars.pop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/javatars/pop/PopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatars.pop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
