7defcd4a77b8de2f01295dee6fd4cd77e0680b19
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-23 16:44:55 +0200

    Added tailwind config
    Co-authored-by: Emiliana <emilianalila@gmail.com>
    Co-authored-by: Kiran Malieakkal <kiranmalieakkal18@gmail.com>
    Co-authored-by: Ivan <ivan.kovals@hotmail.com>
    Co-authored-by: Harald Börestam <harald.borestam@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,18 @@
-import './App.css'
-import LoginButton from './components/LoginButton'
-import LogoutButton from './components/LogoutButton'
-import Profile from './components/Profile'
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 function App() {
-  
-
   return (
     <>
       <main className="container">
-        <h1>Auth0 Login</h1>
-       <LoginButton />
-       <LogoutButton />
-       <Profile />
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Auth0Provider } from '@auth0/auth0-react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN as string;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID as string;
 
 if (!domain || !clientId) {
-  throw new Error('Auth0 environment variables are missing');
+  throw new Error("Auth0 environment variables are missing");
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-   /*  authorizationParams={{
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      /*  authorizationParams={{
       redirect_uri: authConfig.redirectUri,
     }} */
     >
-    <App />
+      <App />
     </Auth0Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
