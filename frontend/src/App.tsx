97f9b93e4a3e2192fd829c45320defdba39039b5
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-24 11:33:28 +0200

    Removing import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/DashBoard";
-
 import Home from "./components/Home";
-import Top_Nav from "./components/Top_Nav";
 import TaxInfo from "./components/TaxInfo";
 
 function App() {
