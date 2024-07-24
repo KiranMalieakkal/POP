63c3e83477d942be756212fbf6faa869fc7965df
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-24 09:40:00 +0200

    Adding Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import LoginButton from "./components/LoginButton";
+import Dashboard from "./components/DashBoard";
+import Home2 from "./components/Home2";
+/* import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
+ */
+
 
 function App() {
   return (
     <>
-      <main className="container">
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-      </main>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home2/>}
+            />
+            <Route
+              path="/dashboard/*"
+              element={<Dashboard />}
+            />
+          </Routes>
+        </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/components/DashBoard.tsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from "react-router-dom";
+import Profile from "./Profile";
+import BottomNav from "./BottomNav";
+
+import Tax from "./Tax";
+import ListReceipts from "./ListReceipts";
+
+
+function Dashboard() {
+
+  // const { data } = useQuery({
+  //   queryKey: ["fetch"],
+  //   queryFn: () =>
+  //     fetch(`http://localhost:3000/api/users/${username}/trips`)
+  //       .then((response) => response.json())
+  //       .then((data) => data),
+  // });
+
+  return (
+    <>
+      <div className="">
+        <Routes>
+          <Route path="receipts" element={<ListReceipts />} />
+          <Route path="addReceipt" element={<ListReceipts  />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="tax" element={<Tax />} />
+        </Routes>
+      </div>
+      <div>
+        <BottomNav />
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
