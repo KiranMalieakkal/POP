ca97f88f7f741e0d56f6503561ad2827ab914aeb
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-24 09:44:51 +0200

    merge conflict resolution

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
 import Dashboard from "./components/DashBoard";
-import Home2 from "./components/Home2";
-/* import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
- */
 
-=======
 import Home from "./components/Home";
-// import Profile from "./components/Profile";
-// import Tax from "./components/Tax";
 import Top_Nav from "./components/Top_Nav";
->>>>>>> main
 
 function App() {
   return (
