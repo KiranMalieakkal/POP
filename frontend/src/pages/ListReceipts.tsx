5f603791eb9d940264e3d12e5bffebcfec25cdab
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-24 16:02:09 +0200

    Added Tanstack Query

--- frontend/src/pages/ListReceipts.tsx
+++ frontend/src/pages/ListReceipts.tsx
@@ -1,9 +1,30 @@
-function ListReceipts(){
-    return (
-        <>
-        <h1>You are at List Receipts Section</h1>
-        </>
-    )
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+function ListReceipts() {
+  const [fetchErrorLog, setfetchErrorLog] = useState("");
+
+  const { data, isError: fetchError } = useQuery({
+    queryKey: ["fetch1"],
+    queryFn: () =>
+      fetch(`https://swapi.dev/api/people/1/`)
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((e) => {
+          setfetchErrorLog(e.message);
+        }),
+  });
+
+  function handleClick(){
+    console.log(data);
+  }
+
+  return (
+    <>
+      <h1>You are at List Receipts Section</h1>
+      <button className="btn btn-primary" onClick={handleClick}>Click me</button>
+    </>
+  );
 }
 
 export default ListReceipts;
