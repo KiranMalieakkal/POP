7d6bce6de3640f3de0c1add76006aaf590ac3dc2
haraldborestam <harald.borestam@gmail.com>
2024-08-01 19:14:30 +0200

    Fix: top nav navigation working. Add: logo

--- frontend/src/components/Top_Nav.tsx
+++ frontend/src/components/Top_Nav.tsx
@@ -1,60 +1,65 @@
-import { Link } from "react-router-dom";
-
-/* import logo from "../assets/logo_pop.jpeg" */
+import { NavLink } from "react-router-dom";
+//import { ReactComponent as Logo } from "../assets/pop-logo-white.svg";
+import Logo from "../assets/pop-logo-white.svg";
 
 const Top_Nav = () => {
-  function handleClick(buttonName: string) {
-    //setActiveButton(buttonName);
-    console.log("button in top nav clicked" + buttonName);
-  }
   return (
-    <>
-      <div className="navbar bg-blue-900">
-        <div className="navbar-start w-20">
-          <a className="text-2xl text-white flex mx-3">POP</a>
-        </div>
-        <div className="navbar-start">
-          <ul className="menu menu-horizontal px-1 text-lg">
-            <li>
-              <Link
-                to="/"
-                className="text-white"
-                onClick={() => handleClick("homee")}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/receipts"
-                className="text-white"
-                onClick={() => handleClick("receipto")}
-              >
-                Receipts
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tax"
-                className="text-white"
-                onClick={() => handleClick("taxas")}
-              >
-                Tax
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/profile"
-                className="text-white"
-                onClick={() => handleClick("profile")}
-              >
-                Profile
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <div className="navbar bg-blue-900 p-0 h-16">
+      <img src={Logo} alt="Logo" className="w-10 h-10 ml-10" />
+      <div className="navbar-start w-20 flex items-center">
+        <a className="text-2xl text-white font-bold mx-3">POP</a>
       </div>
-    </>
+      <div className="navbar-start flex h-full">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `flex items-center h-full px-4 ${
+              isActive
+                ? "text-blue-300 border-t-8 border-blue-300"
+                : "text-white hover:text-blue-300"
+            }`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/receipts"
+          className={({ isActive }) =>
+            `flex items-center h-full px-4 ${
+              isActive
+                ? "text-blue-300 border-t-8 border-blue-300"
+                : "text-white hover:text-blue-300"
+            }`
+          }
+        >
+          Receipts
+        </NavLink>
+        <NavLink
+          to="/tax"
+          className={({ isActive }) =>
+            `flex items-center h-full px-4 ${
+              isActive
+                ? "text-blue-300 border-t-8 border-blue-300"
+                : "text-white hover:text-blue-300"
+            }`
+          }
+        >
+          Tax
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            `flex items-center h-full px-4 ${
+              isActive
+                ? "text-blue-300 border-t-8 border-blue-300"
+                : "text-white hover:text-blue-300"
+            }`
+          }
+        >
+          Profile
+        </NavLink>
+      </div>
+    </div>
   );
 };
 
