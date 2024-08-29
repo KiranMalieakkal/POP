39279501c69c712487dd1a2c71104c539bdeb58b
haraldborestam <harald.borestam@gmail.com>
2024-08-29 10:56:33 +0200

    Style: right hand guide pages now with line art images and shorter text

--- frontend/src/components/ReceiptGuide.tsx
+++ frontend/src/components/ReceiptGuide.tsx
@@ -1,18 +1,15 @@
+import lineArtImage from "../assets/receipt-and-hand-line-art.png";
+
 function ReceiptGuide() {
   return (
     <>
       <div className="text-gray-400 px-5 max-w-md">
-        <p className="pb-2">Get started with receipts</p>
-        <p className="text-left  pb-5">
-          This is where all your receipts live. Start by clicking the blue
-          button. Add and categorize as much as you want.
+        <img src={lineArtImage} alt="Logo" className="" />
+        <p className="text-left  pt-10">
+          This is where all your receipts live. Get started by adding new
+          receipts. Add an image of a receipt and we will identify the text
+          automatically.
         </p>
-        <p className="py-2">Tip</p>
-        <p className="text-left pb-5">
-          When you're ready for something more advanced, feel free to check out
-          the tax section. It's not as scary as it sounds.
-        </p>
-        <p className="underline">contact us</p>
       </div>
     </>
   );
